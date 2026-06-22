04a710b19dba9d9b3497b8ce93c64706b0e1e8d2
Muhammadbilol Qo'chqorov <mblabx@gmail.com>
2026-06-22 22:18:02 +0500

    Update task.py

--- task.py
+++ task.py
@@ -1,2 +1,10 @@
-dfsg
-dfbgsfnht
+balance = 1000
+try:
+ money = int(input("Pul yechish: "))
+ if money > balance:
+ print("Yetarli mablag‘ yo‘q")
+ else:
+ balance -= money
+ print("Qoldiq:", balance)
+except ValueError:
+ print("Faqat son kiriting")
